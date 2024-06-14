0c0d60c805f0100de87bff21b0a0d76e3bbfe678
Ajibola <alaomiyaki17@gmail.com>
2024-06-14 08:39:14 +0100

    worked on header

--- src/components/ActiveLink.jsx
+++ src/components/ActiveLink.jsx
@@ -3,6 +3,6 @@ import Link from 'next/link'
 export default function ActiveLink({ href, children,  activeTab, setActiveTab }) {
 
   return (
-    <Link href={href} onClick={()=>setActiveTab(href)} className={activeTab === href ?"header-link active" : "header-link"}>{children}</Link>
+    <Link href={href} className={activeTab === href ?"header-link active" : "header-link"}>{children}</Link>
   )
 }
