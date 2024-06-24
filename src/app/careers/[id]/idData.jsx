ff8413a7b793d039de36922c458a332b519f78d9
unknown <ibrahimtaiwoti@gmail.com>
2024-06-24 01:17:41 +0100

    dynamic careers page

---
+++ src/app/careers/[id]/idData.jsx
@@ -0,0 +1,18 @@
+export const idData = [
+  {
+    id: 1,
+    jobDescription: "Civil Engineer",
+  },
+  {
+    id: 2,
+    jobDescription: "Line Controller",
+  },
+  {
+    id: 3,
+    jobDescription: "Train Driver",
+  },
+  {
+    id: 4,
+    jobDescription: "Station Operationd Maintenance",
+  },
+]
--- src/components/position/Position.jsx
+++ src/components/position/Position.jsx
@@ -1,17 +1,20 @@
+"use client"
+
 import React from 'react'
+import Link from 'next/link'
 import "./Position.css"
 
-function Position(props) {
+function Position({ job }) {
   return (
     <div className="position__container">
       <div className="position__container-left">
-        <h2>{props.jobDescription}</h2>
-        <small>{props.jobDuration}</small>
+        <h2>{job.jobDescription}</h2>
+        <small>{job.jobDuration}</small>
       </div>
       <div className="position__container-right">
-        <p>{props.location}</p>
+        <p>{job.location}</p>
       <div className="position__container-right_link">
-        <p className="position-green">View Details</p>
+        <Link href={`/careers/${(job.jobDescription)}`} className="position-green">View Details</Link>
         <img src="/arrow2.png" alt=""/>
       </div>
       </div>
