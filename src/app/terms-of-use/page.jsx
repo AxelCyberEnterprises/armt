0a938cba6ecdd67a05d231a94cc99fb729e32e4f
unknown <ibrahimtaiwoti@gmail.com>
2024-07-09 02:25:09 +0100

    little animations addded

--- src/app/terms-of-use/page.jsx
+++ src/app/terms-of-use/page.jsx
@@ -1,12 +1,25 @@
+"use client"
+
 import Bullet from '@/components/Bullet/Bullet'
+import { motion, useScroll, useSpring } from "framer-motion";
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/header/Header'
 import React from 'react'
 import "./page.css"
 
 function page() {
+
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
     <div className="terms__container">
+       <motion.div className="progress-bar" style={{ scaleX }} />
+
       <div className="terms__container-header">
         <Header activeLink={"/terms-of-use"} />
       </div>
